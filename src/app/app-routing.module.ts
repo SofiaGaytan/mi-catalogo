8859e126b08b0d26c923cc15c820171adfce75b4
Sofía Gaytán <sofia.gaytandz@uanl.edu.mx>
2021-10-29 18:02:47 -0500

    E3 A#5,6y7
    Etapa 3
    Actividades 5, 6 y 7

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListComponent } from './list/list.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { TableComponent } from './table/table.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', 
+    redirectTo: '/table', 
+    pathMatch: 'full'
+  },
+  {
+    path: 'list',
+    component: ListComponent
+  },
+  {
+    path:'table',
+    component: TableComponent
+  },
+  {
+    path:'**',
+    component: PageNotFoundComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
