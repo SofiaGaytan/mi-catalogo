f19490cb41b2bf12933246029d9acf67625dea86
Sofía Gaytán <sofia.gaytandz@uanl.edu.mx>
2021-11-06 23:48:43 -0600

    E5 IncidenciaResuelta y A#13.5
    Etapa 5
    Resolución de Incidencias
    Actividad 13.5

--- src/app/modals/modal-add-update/modal-add-update.component.ts
+++ src/app/modals/modal-add-update/modal-add-update.component.ts
@@ -13,13 +13,30 @@ export class ModalAddUpdateComponent implements OnInit {
   accion: string = "";
   auto: Automovil = {_id:0, marca:"",submarca:"",modelos:[],descripcion:""};
   lectura: boolean = false;
+  
+  inicio: number = 2000;
+  fin: number = 2020;
 
   constructor(public activeModal: NgbActiveModal) { }
 
   ngOnInit(): void {
     if(this.accion == "Editar"){
       this.lectura = true;
+      let modelos: Number[] = this.auto.modelos;
+      modelos.sort()
+      this.inicio = parseInt(modelos[0].toString());
+      this.fin = parseInt(modelos[modelos.length - 1].toString())
     }
+  }
+
+  getModelos(){
+    this.auto.modelos = Array.from({length: this.fin - this.inicio + 1 }).map((modelo, i)=>Number(i+this.inicio))
+    console.log(this.auto.modelos)
+  }
+
+  onSubmit(){
+    this.getModelos();
+    this.activeModal.close(this.auto);
   }
 
 }
